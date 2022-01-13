71439577a4089eb56f4e920009c0bdfffc2ae8dd
chandansaini512 <95668378+chandansaini512@users.noreply.github.com>
2022-01-13 09:05:19 +0530

    Assignment10

---
+++ Assignment10/java/Company.java
@@ -0,0 +1,28 @@
+package testing;
+
+public class Company {
+
+	public int registration_number;
+	public String name;
+	public String Created_date;
+	
+	public int getRegistration_number() {
+		return registration_number;
+	}
+	public void setRegistration_number(int registration_number) {
+		this.registration_number = registration_number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCreated_date() {
+		return Created_date;
+	}
+	public void setCreated_date(String created_date) {
+		Created_date = created_date;
+	}
+	
+}
---
+++ Assignment10/java/Employee.java
@@ -0,0 +1,33 @@
+package testing;
+
+public class Employee {
+		public int code;
+		public String name;
+		public String designation;
+		public String joining_date;
+		
+		public int getCode() {
+			return code;
+		}
+		public void setCode(int code) {
+			this.code = code;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getDesignation() {
+			return designation;
+		}
+		public void setDesignation(String designation) {
+			this.designation = designation;
+		}
+		public String getJoining_date() {
+			return joining_date;
+		}
+		public void setJoining_date(String joining_date) {
+			this.joining_date = joining_date;
+		}
+}
